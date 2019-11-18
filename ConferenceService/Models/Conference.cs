bf6789142cb881432f547ca040ff247c1bf7743b
Simon Lindblad <simon.lindblad93@gmail.com>
2019-11-18 17:49:49 +0100

    Add initial data model over conferences (#5)
    * Add data model
    
    * Add mongo db driver
    
    * Document properties
    
    * Add newlines
    
    * Add date time serialization
    
    * Remove date time formatting

---
+++ ConferenceService/Models/Conference.cs
@@ -0,0 +1,55 @@
+﻿namespace ConferenceService.Models
+{
+    using MongoDB.Bson.Serialization.Attributes;
+    using MongoDB.Bson;
+    using System;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Data model representing a conference.
+    /// </summary>
+    public class Conference
+    {
+        /// <summary>
+        /// A unique ID for each conference.
+        /// </summary>
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        /// <summary>
+        /// The name of the conference.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Conference start date.
+        /// </summary>
+        public DateTime StartDate { get; set; }
+
+        /// <summary>
+        /// Conference end date.
+        /// </summary>
+        public DateTime EndDate { get; set; }
+
+        /// <summary>
+        /// The venue that holds the conference.
+        /// </summary>
+        public Venue Venue { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Conference conference &&
+                   Id == conference.Id &&
+                   Name == conference.Name &&
+                   StartDate == conference.StartDate &&
+                   EndDate == conference.EndDate &&
+                   EqualityComparer<Venue>.Default.Equals(Venue, conference.Venue);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id, Name, StartDate, EndDate, Venue);
+        }
+    }
+}
---
+++ ConferenceService/Models/Venue.cs
@@ -0,0 +1,18 @@
+﻿namespace ConferenceService.Models
+{
+    /// <summary>
+    /// Class representing a conference venue.
+    /// </summary>
+    public class Venue
+    {
+        /// <summary>
+        /// The name of the Venue.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The specific address of the venue.
+        /// </summary>
+        public Address Address { get; set; }
+    }
+}
