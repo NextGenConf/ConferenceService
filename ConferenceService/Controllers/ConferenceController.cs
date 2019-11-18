de40430cfad8d56d5b33ad2a276dec0eafedeac4
Simon Lindblad <simon.lindblad93@gmail.com>
2019-11-18 17:52:59 +0100

    Remove unused methods

--- ConferenceService/Controllers/ConferenceController.cs
+++ ConferenceService/Controllers/ConferenceController.cs
@@ -30,23 +30,5 @@
         {
             return conferenceService.GetById(id);
         }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
     }
 }
