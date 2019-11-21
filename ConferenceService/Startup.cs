f80a3bc6eff0642a0b6375b2ea9b61f6a07aefa4
Oliver Wheeler <oliver.kuruma@gmail.com>
2019-11-21 19:12:45 +0100

    Remove https redirection (#20)
    use aspnet instead of sdk for smaller image

--- ConferenceService/Startup.cs
+++ ConferenceService/Startup.cs
@@ -34,13 +34,7 @@
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
 
-            app.UseHttpsRedirection();
             app.UseMvc();
         }
     }
