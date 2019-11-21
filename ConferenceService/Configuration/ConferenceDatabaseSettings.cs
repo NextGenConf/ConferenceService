5fb4bd009016e3ef20485d5f675e74dc60b7a992
Simon Lindblad <simon.lindblad93@gmail.com>
2019-11-21 21:35:15 +0100

    Move host, port, parameters to environment variables (#21)

--- ConferenceService/Configuration/ConferenceDatabaseSettings.cs
+++ ConferenceService/Configuration/ConferenceDatabaseSettings.cs
@@ -8,14 +8,20 @@
     /// </summary>
     public class ConferenceDatabaseSettings
     {
-        private const string MongoDbHostDefault = "localhost:27017";
+        private const string MongoDbHostDefault = "localhost";
+        private const string MongoDbPortDefault = "27017";
         private const string MongoDbHostEnvironmentVar = "MONGO_DB_HOST";
         private const string MongoDbPasswordEnvironmentVar = "MONGO_DB_PASSWORD";
         private const string MongoDbUserEnvironmentVar = "MONGO_DB_USER";
+        private const string MongoDbPortEnvironmentVar = "MONGO_DB_PORT";
+        private const string MongoDbParametersEnvironmentVar = "MONGO_DB_PARAMETERS";
 
         private string mongoUser;
         private string mongoPassword;
         private string mongoHostEnvironment;
+        private string mongoPort;
+        private string mongoHost;
+        private string mongoParameters;
 
         public string AuthString
         {
@@ -31,16 +37,41 @@
             }
         }
 
-        public string ConnectionString
+        public string Port
+        {
+            get
+            {
+                if (mongoPort == null)
+                {
+                    mongoPort = Environment.GetEnvironmentVariable(MongoDbPortEnvironmentVar);
+                }
+
+                return mongoPort == null ? MongoDbPortDefault : mongoPort;
+            }
+        }
+
+        public string Host
         { 
             get 
             {
-                if (mongoHostEnvironment == null)
-                {
-                    mongoHostEnvironment = Environment.GetEnvironmentVariable(MongoDbHostEnvironmentVar);
+                if (mongoHost == null) {
+                    mongoHost = Environment.GetEnvironmentVariable(MongoDbHostEnvironmentVar);
                 }
 
-                return mongoHostEnvironment == null ? MongoDbHostDefault : mongoHostEnvironment;
+                return mongoHost == null ? MongoDbHostDefault : mongoHost;
+            }
+        }
+
+        public string Parameters
+        {
+            get
+            {
+                if (mongoParameters == null) 
+                {
+                    mongoParameters = Environment.GetEnvironmentVariable(MongoDbParametersEnvironmentVar);
+                }
+
+                return mongoParameters == null ? "" : $"/?{mongoParameters}";
             }
         }
 
